b7ac06a0a687e7aabd763ac9dcaaf8dba127c013
Milena Kari <milenakari@yahoo.com>
2019-08-25 02:28:10 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,18 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-let index = 0;
-function init(e) {
+
+function init() {
   // Write your JavaScript code inside the init() 
-  const key = parseInt(e.detail || e.which);
-  if (key === code[index]) {
-    index++;
-    if (index === code.length) {
-      alert("Hurray!");
-      index = 0;
-    } else {
-      index = 0;
+  const body = document.querySelect('body');
+  body.addEventListener('keydown', function(e) {
+    let index = 0;
+    const key = parseInt(e.detail || e.which);
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        alert("Hurray!");
+        index = 0;
+      } else {
+        index = 0;
+      }
     }
-  }
+});
 }
 
-const body = document.querySelect('body');
-body.addEventListener('keydown', init(e));
+init()
