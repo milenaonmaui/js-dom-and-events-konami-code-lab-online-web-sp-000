2fd9caad280ab4605c9da063845ec5a17b34ea8f
Milena Kari <milenakari@yahoo.com>
2019-08-25 02:19:08 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 function init(e) {
   // Write your JavaScript code inside the init() 
+  const key = parseInt(e.detail || e.which);
   
 }
